a0410c519bedec3fc7bdd661af55a8bf0cc7fa45
松下裕紀 <j.hiroki.matsushita@gmail.com>
2017-06-12 20:40:58 +0900

    git動作確認用ファイルの追加

---
+++ src/com/lab/git/test/Test.java
@@ -0,0 +1,11 @@
+package com.lab.git.test;
+
+public class Test {
+
+    public final static String TEST_1 = "1";
+    public final static String TEST_2 = "2";
+    public final static String TEST_3 = "3";
+    public final static String TEST_4 = "4";
+    public final static String TEST_5 = "5";
+    
+}
